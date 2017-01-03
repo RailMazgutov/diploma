fad6efc10187e8dda43b30348e4d7162a720deb8
Rail Mazgutov <rail.mazgutov@gmail.com>
2017-01-03 21:25:49 +0300

    Начал работу с простарнствами имен для OPC UA

--- OPCUA/OPCUA/OPCUA.cpp
+++ OPCUA/OPCUA/OPCUA.cpp
@@ -3,4 +3,10 @@
 
 #include "stdafx.h"
 
+#include "opc_ua.h"
 
+const OPCUA_Namespace* add_namespace(char* name)
+{
+	OPCUA_Namespace opcua_namespace;
+	return &opcua_namespace;
+}
--- OPCUA/OPCUA/opc_ua.h
+++ OPCUA/OPCUA/opc_ua.h
@@ -1,5 +1,23 @@
 #pragma once
-enum OPCUA_DATA_TYPE
+#define NAME_MAX_LENGTH 100
+typedef bool Boolean;
+typedef unsigned char Byte;
+typedef double Double;
+typedef float Float;
+typedef short Int16;
+typedef int Int32;
+typedef long int Int64;
+typedef char* LocalText;
+typedef char sByte;
+typedef unsigned short UInt16;
+typedef unsigned int UInt32;
+
+const bool debug					= false;
+//const char* namespace_name			= "http://eniko.ru";
+//const char* endpoint				= "opc.tcp://localhost:4856/opcua/server";
+//const char* server_uri				= "urn://eniko.ru";
+
+enum opcua_data_type
 {
 	OPCUA_BOOLEAN,
 	OPCUA_BYTE,
@@ -13,3 +31,40 @@ enum OPCUA_DATA_TYPE
 	OPCUA_UNIT16,
 	OPCUA_UINT32
 };
+
+struct OPCUA_Variable
+{
+	union opcua_value
+	{
+		Boolean				bool_val;
+		Byte				byte_val;
+		Double				double_val;
+		Float				float_val;
+		Int16				int16_val;
+		Int32				int32_val;
+		Int64				int64_val;
+		LocalText			local_text_val;
+		sByte				sbyte_val;
+		UInt16				uint16_val;
+		UInt32				uint32_val;
+	};
+
+	//Для представления значения 
+	opcua_data_type type;
+	opcua_value value;
+
+	//Для идентификации
+	unsigned int id;
+	unsigned int namespace_id;
+	//Для представления пользователю
+	char name[NAME_MAX_LENGTH];
+};
+
+struct OPCUA_Namespace
+{
+	//Для идентификации
+	unsigned int id;
+	char name[NAME_MAX_LENGTH];
+};
+
+const OPCUA_Namespace* create_namespace(char* name);
